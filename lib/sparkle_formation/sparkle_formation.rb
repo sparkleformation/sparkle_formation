fda07596e37728a8e0e662f9cd10cdd739d44a18
Chris Roberts <chrisroberts.code@gmail.com>
2013-07-08 23:50:35 -0700

    Add support for dynamics and arbitrary insertion

--- lib/sparkle_formation/sparkle_formation.rb
+++ lib/sparkle_formation/sparkle_formation.rb
@@ -1,11 +1,15 @@
 require 'chef/mash'
 require 'attribute_struct'
+require 'knife-cloudformation/sparkle_attribute'
 
 AttributeStruct.camel_keys = true
 
 module KnifeCloudformation
   class SparkleFormation
     class << self
+
+      attr_reader :dynamics
+      
       def compile(path)
         formation = self.instance_eval(IO.read(path), path, 1)
         formation.compile._dump
@@ -21,6 +25,31 @@ module KnifeCloudformation
         self.instance_eval(IO.read(path), path, 1)
       end
 
+      def load_dynamics!(directory)
+        @loaded_dynamics ||= []
+        Dir.glob(File.join(directory, '*.rb')).each do |dyn|
+          dyn = File.expand_path(dyn)
+          next if @loaded_dynamics.include?(dyn)
+          self.instance_eval(IO.read(dyn), dyn, 1)
+          @loaded_dynamics << dyn
+        end
+        @loaded_dynamics.uniq!
+        true
+      end
+      
+      def dynamic(name, &block)
+        @dynamics ||= Mash.new
+        @dynamics[name] = block
+      end
+
+      def insert(dynamic_name, struct, *args)
+        if(@dynamics && @dynamics[dynamic_name])
+          struct.instance_exec(*args, &@dynamics[dynamic_name])
+          struct
+        else
+          raise "Failed to locate requested dynamic block for insertion: #{dynamic_name} (valid: #{@dynamics.keys.sort.join(', ')})"
+        end
+      end
     end
     
     attr_reader :name
@@ -31,6 +60,8 @@ module KnifeCloudformation
     def initialize(name, options={})
       @name = name
       @sparkle_path = options[:sparkle_path] || File.join(Dir.pwd, 'cloudformation/components')
+      @dynamics_directory = options[:dynamics_directory] || File.join(File.dirname(@sparkle_path), 'dynamics')
+      self.class.load_dynamics!(@dynamics_directory)
       @components = Mash.new
       @load_order = []
     end
