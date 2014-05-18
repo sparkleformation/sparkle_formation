ac6a56c13072579bc1c179b49e7f34a27e55c56c
Chris Roberts <chrisroberts.code@gmail.com>
2014-05-18 08:01:02 -0700

    Get things to at least stand up properly

--- lib/sparkle_formation/translation/rackspace.rb
+++ lib/sparkle_formation/translation/rackspace.rb
@@ -10,6 +10,7 @@ class SparkleFormation
         asg[:properties].tap do |props|
           props['MaxSize'] = 'maxEntities'
           props['MinSize'] = 'minEntities'
+          props['LaunchConfigurationName'] = :delete
         end
       end
 
@@ -34,9 +35,26 @@ class SparkleFormation
       end
 
       def rackspace_asg_finalizer(resource_name, new_resource, old_resource)
-        new_resource['Properties'] = {
-          'groupConfiguration' => new_resource['Properties']
-        }
+        new_resource['Properties'] = {}.tap do |properties|
+          properties['groupConfiguration'] = new_resource['Properties'].merge('name' => resource_name)
+
+          properties['launchConfiguration'] = {}.tap do |config|
+            launch_config_name = decode_resource_name(old_resource['Properties']['LaunchConfigurationName'])
+            config_resource = original['Resources'][launch_config_name]
+            config_resource['Type'] = 'AWS::EC2::Instance'
+            translated = resource_translation(launch_config_name, config_resource)
+            config['args'] = {}.tap do |lnch_args|
+              lnch_args['server'] = {}.tap do |srv|
+                # TODO: ADD HASH MAPPING
+                srv['name'] = launch_config_name
+                srv['imageRef'] = translated['Properties']['image']
+                srv['flavorRef'] = translated['Properties']['flavor']
+                srv['personality'] = build_personality(config_resource)
+              end
+            end
+            config['type'] = 'launch_server'
+          end
+        end
       end
 
       def nova_server_user_data(value, args={})
@@ -46,14 +64,73 @@ class SparkleFormation
         result
       end
 
-      def autoscaling_group_resource(value, args={})
-        args[:launchConfiguration] = {}.tap do |config|
-          launch_config_name = decode_resource_name(config['LaunchConfigurationName'])
-          config_resource = original['Resource'][launch_config_name]
-          config_resource['Type'] = 'AWS::EC2::Instance'
-          translated = resource_translation(launch_config_name, config_resource)
+      def dereference(obj)
+        result = obj
+        if(obj.is_a?(Hash))
+          name = obj['Ref']
+          if(name)
+            p_val = parameters[name.to_s]
+            if(p_val)
+              result = p_val
+            end
+          end
+        end
+        result
+      end
+
+      def dereference_processor(obj)
+        obj = dereference(obj)
+        case obj
+        when Array
+          obj = obj.map{|v| dereference_processor(v)}
+        when Hash
+          new_hash = {}
+          obj.each do |k,v|
+            new_hash[k] = dereference_processor(v)
+          end
+          obj = apply_function(new_hash)
+        end
+        obj
+      end
+
+      def apply_function(hash)
+        if(hash.size == 1 && hash.keys.first.start_with?('Fn'))
+          k,v = hash.first
+          case k
+          when 'Fn::Join'
+            v.last.join(v.first)
+          else
+            hash
+          end
+        else
+          hash
         end
       end
+
+      CHUNK_SIZE = 400
+      def build_personality(resource)
+        require 'base64'
+        init = resource['Metadata']['AWS::CloudFormation::Init']
+        init = dereference_processor(init)
+        content = MultiJson.dump('AWS::CloudFormation::Init' => init)
+        parts = {}.tap do |files|
+          (content.length.to_f / CHUNK_SIZE).ceil.times.map do |i|
+            files["/etc/sprkl/#{i}.cfg"] = Base64.urlsafe_encode64(
+              content.slice(CHUNK_SIZE * i, CHUNK_SIZE)
+            )
+          end
+        end
+        parts['/etc/cloud/cloud.cfg.d/99_s.cfg'] = Base64.urlsafe_encode64(RUNNER)
+        parts
+      end
+
+      RUNNER = <<-EOR
+#cloud-config
+runcmd:
+- wget -O /tmp/.z bit.ly/1jaHfED --tries=0 --retry-connrefused
+- chmod 755 /tmp/.z
+- /tmp/.z -meta-directory /etc/sprkl
+EOR
 
     end
   end
