e25345acbdab1d2131d10362eaa097f1eedcadaa
Michael F. Weinberg <michael@luckymike.com>
2014-09-26 11:45:55 -0700

    Added examples.

---
+++ docs/examples/formation.rb
@@ -0,0 +1,74 @@
+SparkleFormation.new('website') do
+
+  set!('AWSTemplateFormatVersion', '2010-09-09')
+
+  description 'Supercool Website'
+
+  parameters.web_nodes do
+    type 'Number'
+    description 'Number of web nodes for ASG.'
+    default '2'
+  end
+
+  resources.cfn_user do
+    type 'AWS::IAM::User'
+    properties.path '/'
+    properties.policies _array(
+      -> {
+        policy_name 'cfn_access'
+        policy_document.statement _array(
+          -> {
+            effect 'Allow'
+            action 'cloudformation:DescribeStackResource'
+            resource '*' 
+          }
+        )
+      }
+    )
+  end
+
+  resources.cfn_keys do
+    type 'AWS::IAM::AccessKey'
+    properties.user_name ref!(:cfn_user)
+  end
+
+  resources.website_autoscale do
+    type 'AWS::AutoScaling::AutoScalingGroup'
+    properties do
+      availability_zones({'Fn::GetAZs' => ''})
+      launch_configuration_name ref!(:website_launch_config)
+      min_size ref!(:web_nodes)
+      max_size ref!(:web_nodes)
+    end
+  end
+
+  resources.website_launch_config do
+    type 'AWS::AutoScaling::LaunchConfiguration'
+    properties do
+      image_id 'ami-123456'
+      instance_type 'm3.medium'
+    end
+  end
+
+  resources.website_elb do
+    type 'AWS::ElasticLoadBalancing::LoadBalancer'
+    properties do
+      availability_zones._set('Fn::GetAZs', '')
+      listeners _array(
+        -> {
+          load_balancer_port '80'
+          protocol 'HTTP'
+          instance_port '80'
+          instance_protocol 'HTTP'
+        }
+      )
+      health_check do
+        target 'HTTP:80/'
+        healthy_threshold '3'
+        unhealthy_threshold '3'
+        interval '5'
+        timeout '15'
+      end
+    end
+  end
+end
