0b7d3eeed9fd223ef49cd6e011ccc29a666b8788
Chris Roberts <chrisroberts.code@gmail.com>
2014-12-18 09:58:26 -0800

    Add in full anchoring to headers. If anchor requested, scroll to anchor after loading.

--- docs/v/loader.js
+++ docs/v/loader.js
@@ -10,8 +10,24 @@ function load_page(){
 
 function display_page(content){
   content = content.replace(/.md/g, '.html');
-  $('#content').html(marked(content));
+  renderer = new marked.Renderer();
+  renderer.heading = function(text, level){
+    escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+
+    return '<h' + level + '><a name="' +
+      escapedText +
+      '" class="anchor" href="#' +
+      escapedText +
+      '"><span class="header-link"></span></a>' +
+      text + '</h' + level + '>';
+  }
+  $('#content').html(marked(content, {renderer: renderer}));
   hljs.initHighlighting();
+  if(window.location.hash){
+    $('html, body').scrollTop(
+      $("a[name='"+ window.location.hash.replace('#', '') + "']").offset().top
+    );
+  }
 }
 
 $(document).ready(load_page);
