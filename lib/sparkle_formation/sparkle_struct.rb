784be288b4530979c0391c3654ef80b2c516fd53
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-26 08:27:37 -0700

    Use proper namespacing for constant access

--- lib/sparkle_formation/sparkle_struct.rb
+++ lib/sparkle_formation/sparkle_struct.rb
@@ -11,17 +11,17 @@ class SparkleFormation
     # @param inst [SparkleFormation]
     # @return [SparkleFormation]
     def _set_self(inst)
-      unless(inst.is_a?(SparkleFormation))
-        raise TypeError.new "Expecting type of `SparkleFormation` but got `#{inst.class}`"
+      unless(inst.is_a?(::SparkleFormation))
+        ::Kernel.raise ::TypeError.new "Expecting type of `SparkleFormation` but got `#{inst.class}`"
       end
       @self = inst
     end
 
     # @return [SparkleFormation]
-    def _self
+    def _self(*_)
       unless(@self)
         if(_parent.nil?)
-          raise 'Creator did not provide return reference!'
+          ::Kernel.raise ::ArgumentError.new 'Creator did not provide return reference!'
         else
           _parent._self
         end
@@ -34,5 +34,6 @@ class SparkleFormation
     def _klass
       ::SparkleFormation::SparkleStruct
     end
+
   end
 end
