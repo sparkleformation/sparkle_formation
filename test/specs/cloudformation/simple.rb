55d13df6adc0bb88fda4248557d89f5ee18f626c
Chris Roberts <chrisroberts.code@gmail.com>
2015-04-06 14:35:57 -0700

    Fix block addition method name

--- test/specs/cloudformation/simple.rb
+++ test/specs/cloudformation/simple.rb
@@ -1,4 +1,4 @@
-SparkleFormation.new('simple').load(:user_info).overloads do
+SparkleFormation.new('simple').load(:user_info).overrides do
   dynamic!(:node, 'fubar')
   outputs.region do
     description 'Region of stack'
