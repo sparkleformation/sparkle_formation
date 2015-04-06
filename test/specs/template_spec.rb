d3804cbf6b062235f4211f51f41cb8b45b51be93
Chris Roberts <chrisroberts.code@gmail.com>
2015-04-06 14:36:09 -0700

    Build templates and validate expected results

--- test/specs/template_spec.rb
+++ test/specs/template_spec.rb
@@ -4,11 +4,30 @@ describe SparkleFormation do
     SparkleFormation.sparkle_path = File.join(File.dirname(__FILE__), 'cloudformation')
   end
 
-  describe 'Simple template' do
+  describe 'Dummy template' do
 
-    it 'should build the template' do
-      SparkleFormation.compile('dummy.rb')
+    it 'should build the dummy template' do
+      result = SparkleFormation.compile('dummy.rb')
+      result.must_be :is_a?, Hash
+      result.to_smash.get('Outputs', 'Dummy', 'Value').must_equal 'Dummy value'
+      result.to_smash.get('Parameters', 'Creator', 'Default').must_equal 'Fubar'
     end
 
   end
+
+  describe 'Nested template' do
+
+    it 'should build the nested template' do
+      result = SparkleFormation.compile('nest.rb')
+      simple = SparkleFormation.compile('simple.rb')
+      dummy = SparkleFormation.compile('dummy.rb')
+      result.must_be :is_a?, Hash
+      simple.must_be :is_a?, Hash
+      dummy.must_be :is_a?, Hash
+      result.to_smash.get('Resources', 'Dummy', 'Properties', 'Stack').to_json.must_equal dummy.to_smash.to_json
+      result.to_smash.get('Resources', 'Simple', 'Properties', 'Stack').to_json.must_equal simple.to_smash.to_json
+    end
+
+  end
+
 end
