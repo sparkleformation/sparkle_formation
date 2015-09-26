b837d3eca66bcff5a9c400e3665315dfdbae3c78
Mat Schaffer <mat@schaffer.me>
2015-09-27 07:24:25 +0900

    Use _no_hump in examples with region names
    Should help avoid confusion for folks browsing deeper into the codebase.

--- examples/ami_component/cloudformation/components/ami.rb
+++ examples/ami_component/cloudformation/components/ami.rb
@@ -8,14 +8,13 @@ SparkleFormation.build do
   end
 
   mappings.region_map do
-    set!('us-east-1', :ami => 'ami-7f418316')
-    set!('us-east-1', :ami => 'ami-7f418316')
-    set!('us-west-1', :ami => 'ami-951945d0')
-    set!('us-west-2', :ami => 'ami-16fd7026')
-    set!('eu-west-1', :ami => 'ami-24506250')
-    set!('sa-east-1', :ami => 'ami-3e3be423')
-    set!('ap-southeast-1', :ami => 'ami-74dda626')
-    set!('ap-northeast-1', :ami => 'ami-dcfa4edd')
+    set!('us-east-1'._no_hump, :ami => 'ami-7f418316')
+    set!('us-west-1'._no_hump, :ami => 'ami-951945d0')
+    set!('us-west-2'._no_hump, :ami => 'ami-16fd7026')
+    set!('eu-west-1'._no_hump, :ami => 'ami-24506250')
+    set!('sa-east-1'._no_hump, :ami => 'ami-3e3be423')
+    set!('ap-southeast-1'._no_hump, :ami => 'ami-74dda626')
+    set!('ap-northeast-1'._no_hump, :ami => 'ami-dcfa4edd')
   end
 
 end
