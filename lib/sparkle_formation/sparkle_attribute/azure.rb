1ec1fd1fa9fa72f3dcc20b0fd3aeb51269486ff1
Chris Roberts <code@chrisroberts.org>
2016-01-13 10:31:11 -0800

    Support auto re-format of resources when provdied as Hash type

--- lib/sparkle_formation/sparkle_attribute/azure.rb
+++ lib/sparkle_formation/sparkle_attribute/azure.rb
@@ -8,9 +8,42 @@ class SparkleFormation
     # Azure specific helper implementations
     module Azure
 
+      # Extract resources Hash from template dump and transform to
+      # Array type expected by the ARM API
+      #
+      # @param hash [Hash] template dump
+      # @return [Hash]
+      def self.resources_formatter(hash)
+        if(hash.key?('resources') && !hash['resources'].is_a?(Array))
+          resources = hash.delete('resources')
+          hash['resources'] = Array.new
+          resources.each do |r_name, r_contents|
+            hash['resources'].push(
+              r_contents.merge('name' => r_name)
+            )
+          end
+        end
+        hash
+      end
+
       # Inject camel style on module inclusion
+      # Add custom dump functionality to properly set resources
       def self.included(klass)
         klass.const_set(:CAMEL_STYLE, :no_leading)
+
+        klass.class_eval do
+          def _azure_dump
+            result = _attribute_struct_dump
+            if(_parent.nil?)
+              result = ::SparkleFormation::SparkleAttribute::Azure.resources_formatter(result)
+            end
+            result
+          end
+          alias_method :_attribute_struct_dump, :_dump
+          alias_method :_dump, :_azure_dump
+          alias_method :dump!, :_azure_dump
+        end
+
       end
 
       # Valid azure builtin functions
