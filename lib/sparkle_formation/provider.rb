0ff76777f2f6147ddb0059eb19e965bfaf697190
Chris Roberts <code@chrisroberts.org>
2016-01-17 08:18:19 -0800

    Include heat provider module

--- lib/sparkle_formation/provider.rb
+++ lib/sparkle_formation/provider.rb
@@ -5,6 +5,7 @@ class SparkleFormation
 
     autoload :Aws, 'sparkle_formation/provider/aws'
     autoload :Azure, 'sparkle_formation/provider/azure'
+    autoload :Heat, 'sparkle_formation/provider/heat'
 
   end
 end
