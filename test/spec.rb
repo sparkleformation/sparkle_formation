db5cbfeb6d8641f39f2ada0d17d197333ed80e14
Chris Roberts <code@chrisroberts.org>
2016-01-18 09:41:58 -0800

    Extract capture helper to root spec helper

--- test/spec.rb
+++ test/spec.rb
@@ -1,6 +1,14 @@
 require 'sparkle_formation'
 require 'minitest/autorun'
 
+def capture_stdout
+  old, $stdout = $stdout, StringIO.new
+  yield
+  $stdout.string
+ensure
+  $stdout = old
+end
+
 SparkleFormation.sparkle_path = File.join(
   File.dirname(__FILE__), 'specs', 'sparkleformation'
 )
