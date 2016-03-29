1c262bc7340b47938f67c739d3f03feebe1d3d31
Chris Roberts <code@chrisroberts.org>
2016-03-29 11:07:50 -0700

    Start adding customized conditionals

--- lib/sparkle_formation/resources/aws.rb
+++ lib/sparkle_formation/resources/aws.rb
@@ -10,10 +10,69 @@ class SparkleFormation
 
       # Conditionals for property updates
       PROPERTY_UPDATE_CONDITIONALS = Smash.new(
+        'AWS::DynamoDB::Table' => {
+          'GlobalSecondaryIndexes' => [
+            UpdateCausesConditional.new('none',
+              lambda{|final, orig|
+              }
+            ),
+            UpdateCausesConditional.new('replacement', true)
+          ]
+        },
+        'AWS::EC2::EIPAssociation' => {
+          'AllocationId' => [
+            UpdateCausesConditional.new('replacement',
+              lambda{|final, original|
+                original.get('Properties', 'InstanceId') != final.get('Properties', 'InstanceId') ||
+                  original.get('Properties', 'NetworkInterfaceId') != final.get('Properties', 'NewtorkInterfaceId')
+              }
+            ),
+            UpdateCausesConditional.new('none', true)
+          ],
+          'EIP' => [
+            UpdateCausesConditional.new('replacement',
+              lambda{|final, original|
+                original.get('Properties', 'InstanceId') != final.get('Properties', 'InstanceId') ||
+                  original.get('Properties', 'NetworkInterfaceId') != final.get('Properties', 'NewtorkInterfaceId')
+              }
+            ),
+            UpdateCausesConditional.new('none', true)
+          ],
+          'InstanceId' => [
+            UpdateCausesConditional.new('replacement',
+              lambda{|final, original|
+                original.get('Properties', 'AllocationId') != final.get('Properties', 'AllocationId') ||
+                  original.get('Properties', 'EIP') != final.get('Properties', 'EIP')
+              }
+            ),
+            UpdateCausesConditional.new('none', true)
+          ],
+          'NetworkInterfaceId' => [
+            UpdateCausesConditional.new('replacement',
+              lambda{|final, original|
+                original.get('Properties', 'AllocationId') != final.get('Properties', 'AllocationId') ||
+                  original.get('Properties', 'EIP') != final.get('Properties', 'EIP')
+              }
+            ),
+            UpdateCausesConditional.new('none', true)
+          ]
+        },
         'AWS::EC2::Instance' => {
+          'AdditionalInfo' => [
+          ],
+          'BlockDeviceMappings' => [
+          ],
+          'EbsOptimized' => [
+          ],
+          'InstanceType' => [
+          ],
+          'KernelId' => [
+          ],
+          'RamdiskId' => [
+          ],
           'SecurityGroupIds' => [
             UpdateCausesConditional.new('none',
-              lambda{|final|
+              lambda{|final, _orig|
                 final.get('Properties', 'SubnetId') ||
                   final.fetch('Properties', 'NetworkInterface', {}).values.include?('SubnetId')
               }
@@ -21,6 +80,58 @@ class SparkleFormation
             UpdateCausesConditional.new('replacement',
               lambda{|*_| 'replacement'}
             )
+          ],
+          'UserData' => [
+          ]
+        },
+        'AWS::EC2::NetworkInterface' => {
+          'PrivateIpAddresses' => [
+          ]
+        },
+        'AWS::ElastiCache::CacheCluster' => {
+          'NumCacheNodes' => [
+          ],
+          'PreferredAvailabilityZones' => [
+          ]
+        },
+        'AWS::ElasticLoadBalancing::LoadBalancer' => {
+          'AvailabilityZones' => [
+          ],
+          'HealthCheck' => [
+          ],
+          'Subnets' => [
+          ]
+        },
+        'AWS::RDS::DBCluster' => {
+          'BackupRetentionPeriod' => [
+          ],
+          'PreferredMaintenanceWindow' => [
+          ]
+        }
+        'AWS::RDS::DBClusterParameterGroup' => {
+          'Parameters' => [
+          ]
+        },
+        'AWS::RDS::DBInstance' => {
+          'AutoMinorVersionUpgrade' => [
+          ],
+          'BackupRetentionPeriod' => [
+          ],
+          'DBParameterGroupName' => [
+          ],
+          'PreferredMaintenanceWindow' => [
+          ]
+        },
+        'AWS::RDS::DBParameterGroup' => {
+          'Parameters' => [
+          ]
+        },
+        'AWS::RDS::EventSubscription' => {
+          'SourceType' => [
+          ]
+        },
+        'AWS::Route53::HostedZone' => {
+          'VPCs' => [
           ]
         }
       )
