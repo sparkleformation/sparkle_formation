17cbbedfbc3eb933371b151feba7b55d5039dc1a
Chris Roberts <code@chrisroberts.org>
2016-04-18 14:42:38 -0700

    Fully disable shallow nesting. Clean output mapping to correct locations.

--- lib/sparkle_formation/provider/google.rb
+++ lib/sparkle_formation/provider/google.rb
@@ -128,40 +128,24 @@ class SparkleFormation
           stack.compile.parameters.keys!.each do |parameter_name|
             if(output_name = output_matched?(parameter_name, outputs.keys))
               next if outputs[output_name] == stack
-              stack_output = stack.make_output_available(output_name, outputs)
-              resource.properties.parameters._set(parameter_name).value stack_output
+              stack_output = stack.make_output_available(output_name, outputs, self)
+              # NOTE: Only set value if not already explicitly set
+              if(resource.properties._set(parameter_name).nil?)
+                resource.properties._set(parameter_name, stack_output)
+              end
             end
           end
         end
         if(block_given?)
           extract_templates(&block)
         end
-        #        dump
         self
       end
 
-      # Apply shallow nesting. This style of nesting will bubble
-      # parameters up to the root stack. This type of nesting is the
-      # original and now deprecated, but remains for compat issues so any
-      # existing usage won't be automatically busted.
-      #
-      # @yieldparam resource_name [String] name of stack resource
-      # @yieldparam stack [SparkleFormation] nested stack
-      # @yieldreturn [String] Remote URL storage for template
-      # @return [SparkleFormation::SparkleStruct] compiled structure
+      # Forcibly disable shallow nesting as support for it with Google templates doesn't
+      # really make much sense.
       def apply_shallow_nesting(*args, &block)
-        parameters = compile.parameters
-        output_map = {}
-        nested_stacks(:with_resource, :with_name).each do |_stack, stack_resource, stack_name|
-          remap_nested_parameters(compile, parameters, stack_name, stack_resource, output_map)
-        end
-        extract_templates(&block)
-        if(args.include?(:bubble_outputs))
-          output_map.each do |o_name, o_val|
-            compile.outputs._set(o_name).value compile._stack_output(*o_val)
-          end
-        end
-        dump
+        raise NotImplementedError.new 'Shallow nesting is not supported for this provider!'
       end
 
       # Extract output to make available for stack parameter usage at the
@@ -169,8 +153,9 @@ class SparkleFormation
       #
       # @param output_name [String] name of output
       # @param outputs [Hash] listing of outputs
+      # @param source_stack [SparkleFormation] requesting stack
       # @reutrn [Hash] reference to output value (used for setting parameter)
-      def make_output_available(output_name, outputs)
+      def make_output_available(output_name, outputs, source_stack)
         bubble_path = outputs[output_name].root_path - root_path
         drip_path = root_path - outputs[output_name].root_path
         bubble_path.each_slice(2) do |base_sparkle, ref_sparkle|
@@ -193,7 +178,7 @@ class SparkleFormation
             "(Output Path: #{outputs[output_name].root_path.map(&:name).join(' > ')} " <<
             "Requester Path: #{root_path.map(&:name).join(' > ')})"
         end
-        result = compile._stack_output(bubble_path.first.name, output_name)
+        result = source_stack.compile._stack_output(bubble_path.first.name, output_name)
         if(drip_path.size > 1)
           parent = drip_path.first.parent
           drip_path.unshift(parent) if parent
@@ -205,55 +190,6 @@ class SparkleFormation
           end
         end
         result
-      end
-
-      # Extract parameters from nested stacks. Check for previous nested
-      # stack outputs that match parameter. If match, set parameter to use
-      # output. If no match, check container stack parameters for match.
-      # If match, set to use ref. If no match, add parameter to container
-      # stack parameters and set to use ref.
-      #
-      # @param template [Hash] template being processed
-      # @param parameters [Hash] top level parameter set being built
-      # @param stack_name [String] name of stack resource
-      # @param stack_resource [Hash] duplicate of stack resource contents
-      # @param output_map [Hash] mapping of output names to required stack output access
-      # @return [TrueClass]
-      # @note if parameter has includes `StackUnique` a new parameter will
-      #   be added to container stack and it will not use outputs
-      def remap_nested_parameters(template, parameters, stack_name, stack_resource, output_map)
-        nested_template = stack_resource.properties.stack.compile
-        stack_parameters = nested_template.parameters
-        unless(stack_parameters.nil?)
-          stack_parameters._keys.each do |pname|
-            pval = stack_parameters[pname]
-            unless(pval.stack_unique.nil?)
-              check_name = [stack_name, pname].join
-            else
-              check_name = pname
-            end
-            if(!parameters._set(check_name).nil?)
-              template.resources._set(stack_name).properties.parameters._set(pname).value(
-                template._parameter(check_name)
-              )
-            elsif(output_map[check_name])
-              template.resources._set(stack_name).properties.parameters._set(pname).value(
-                template._stack_output(*output_map[check_name])
-              )
-            else
-              parameters._set(check_name, pval)
-              template.resources._set(stack_name).properties.parameters._set(pname).value(
-                template._parameter(check_name)
-              )
-            end
-          end
-        end
-        unless(nested_template.outputs.nil?)
-          nested_template.outputs.keys!.each do |oname|
-            output_map[oname] = [stack_name, oname]
-          end
-        end
-        true
       end
 
     end
